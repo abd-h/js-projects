7f35ce04a1fa45b97b96a57e08cdf2f2260b8136
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-10-24 21:42:29 +0100

    added Passinger counter app

--- passenger-counter-app/app.js
+++ passenger-counter-app/app.js
@@ -5,26 +5,46 @@
 // increment the count variable when the button is clicked; the easiers way is defining in the html
 // change the count-el in the HTML to releflect the new count
 
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el");
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el");
+
+// let count = 0;
+
+
+// function increment() {
+//     count += 1;
+//     console.log(countEl.textContent = count);
+//      return countEl.textContent = count;
+// } 
+// console.log(increment());
+// console.log(count);
+
+// function save() {
+//     let saveCount = ` ${count} -`
+//     saveEl.textContent += saveCount;
+//     countEl.textContent = 0
+//     count = 0;
+//     // saveCount = 0;
+    
+// }
+// console.log(save());
+
 
 let count = 0;
 
+let countEl = document.getElementById('count-el')
+let incrementBtn = document.getElementById("increment-btn")
+let saveEl = document.getElementById("save-el")
+let saveBtn = document.getElementById("save-btn")
+incrementBtn.addEventListener('click', () => {
+    count++
+  countEl.textContent =  count;
+})
 
-function increment() {
-    count += 1;
-    console.log(countEl.textContent = count);
-     return countEl.textContent = count;
-} 
-console.log(increment());
-console.log(count);
-
-function save() {
-    let saveCount = ` ${count} -`
-    saveEl.textContent += saveCount;
-    countEl.textContent = 0
+saveBtn.addEventListener("click", () => {
+    saveEl.textContent += ` ${count} -`
     count = 0;
-    // saveCount = 0;
+    countEl.textContent = 0;
     
-}
-console.log(save());
+})
+
---
+++ rendering-error-message/index.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('#button').addEventListener('click', ()=> {
+    let error = document.getElementById("error");
+    console.log(error.textContent = `Sorry this item is currently out of stock`);
+    return error.textContent = `Sorry this item is currently out of stock`
+})
+console.log(button);
