8edc69a1cbca9ad49273e7779edd30119a4d428d
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-11-01 18:14:20 +0000

    added blackjack game

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,6 +1,7 @@
 /*      Psudocode for blackjack game
  1.     Create two veriables, firstCard and secondCard.
         Set their values to a random number between 2 - 11
+        Declare  array - cards - that contain firstCard and secondCard
 
  2.     Create a variable, and set it to the sum of the two cards.
 
@@ -27,16 +28,20 @@ empty string.
 12. render the cards on the page using this format; Cards: 21
 
 13. Declare newCard variable and addEventListenern 
+     
 
 14. Create startGame function and then evoke it.
 
 15. Declare avariab call cards and should an array containing cards.
+
+16.  Create for loop that renders out all the cards instead of just 2
+
+17. Create getRandomCard that always returns numbers between 2 - 11 and assign it to firstCard and secondCard 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
-let firstCard = Math.floor(Math.random() * 11) + 2,
-     secondCard = Math.floor(Math.random() * 11) + 2,
-     cards = [firstCard, secondCard],
-     sum = firstCard + secondCard,
+let 
+    cards = [],
+     sum = 0,
      hasBlackJack = false,
      isAlive = true,
      message = ""
@@ -46,33 +51,56 @@ let startBtn = document.querySelector("button"),
     sumEl = document.querySelector("#sum-el"),
     cardsEl = document.querySelector("#cards-el")
     newCard = document.querySelector(".new-card-el")
-  
+    
+    function getRandomCard() {
+        let random = Math.floor(Math.random() * 13) + 1
+        if(random > 10){
+            return 11;
+        }
+        else if(random === 1){
+            return 11;
+        }
+        return random
+    } 
     function startGame() {
+     let   firstCard = getRandomCard(),
+            secondCard = getRandomCard();
+        cards.push(firstCard, secondCard);
+        sum +=  (firstCard + secondCard) + thirdCard;  
+
+     
         return renderGame()
     }
-function renderGame() {
-  startBtn.addEventListener("click", () =>{
-    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`
-    sumEl.textContent =`Sum: ${sum}`;
-   (sum < 21)? message = `You're still in the game, do you want to draw another card`
-   :(sum === 21)? hasBlackJack = true? message = `Congratulatlion You are the winner`
-   :isAlive = false:
-    message = `Game is over`;
-    messageEl.textContent = message
-    console.log(hasBlackJack)
-    console.log(isAlive)
-})
-}
-renderGame()
 
-
-
-newCard.addEventListener("click", () => {
-    let thirdCard = Math.floor(Math.random() * 11) + 2;
-    cardsEl.textContent = `Cards: ${thirdCard}`
-    sum += thirdCard
+    function renderGame() {
+        startBtn.addEventListener("click", () =>{
+            cardsEl.textContent = `Cards: `
+            for(let i = 0; i < cards.length; i++){
+                cardsEl.textContent += ` ${cards[i]} `
+            }
+            sumEl.textContent =`Sum: ${sum}`;
+            (sum < 21)? message = `You're still in the game, do you want to draw another card`
+            :(sum === 21)? hasBlackJack = true? message = `Congratulatlion You are the winner`
+            :isAlive = false:
+            message = `Game is over`;
+            messageEl.textContent = message
+            console.log(hasBlackJack)
+            console.log(isAlive)
+        })
+    }
     renderGame()
-})
 
 
+
+    newCard.addEventListener("click", () => {
+        let thirdCard = getRandomCard();
+        cardsEl.textContent = `Cards: ${thirdCard}`
+        sum += thirdCard
+        cards.push(thirdCard)
+        
+        renderGame()
+    })
+
+/*
+   */
 
--- js-practice-sheet.js/index.js
+++ js-practice-sheet.js/index.js
@@ -23,3 +23,107 @@ function age100() {
     }
 }
 console.log(age100());
+/*:::::::::::::::::::::::::::::::::::::::::::::*/
+// Arrays practice
+let describeYourSelf = [
+    "Joseph", 
+    45, 
+    {"Likes Pizz": false}
+]
+
+// Push the new message array, and then log out  the array
+
+let messages = [
+    "Hey, how is it going!",
+    "I'm great thank you?",
+    "All good, thank you",
+    "I've been working on my portifolio"
+]
+
+let message = "Same here!" 
+messages.push(message)
+console.log(messages);
+messages.pop(message)
+console.log(messages);
+
+// For loops
+ for (let i = 2; i < 11; i++){
+     console.log(i);
+ }
+
+messages.push(message)
+
+for (let i = 0; i < messages.length; i++) {
+  
+    console.log(messages[i]);
+}
+
+let sentence = ['Hello', 'my', 'name', 'is', 'Jason']
+    greetingEl = document.querySelector("#greeting-el");
+
+for (let i = 0; i < sentence.length; i++) {
+    greetingEl.textContent += `${sentence[i]} `;
+}   
+
+// Returning values in functions
+
+let player1Time = 102,
+    player2Time = 101;
+ 
+function fasterstPlayerTime() {
+        if(player1Time < player2Time) {
+            return `Player 1 is the winner`
+        } else if(player1Time === player2Time){
+            return "Its a draw";
+        } else {
+            return `Player 2 is a winner`    
+        }
+    }  
+    let fasterstime = fasterstPlayerTime()
+    console.log(fasterstime);  
+
+    function getTotalRaceTime() {
+        return player1Time + player2Time
+    }
+    let totalTime = getTotalRaceTime();
+    console.log(totalTime);
+
+    console.log();
+
+    function rollDice() {
+        let MathFloored = Math.floor(Math.random()  * 6) + 1 
+        return MathFloored
+    }
+    console.log(rollDice());
+
+    // Logical operators
+
+    let hasCompletedCourse = true,
+        givesCertificate = true;
+
+        if(hasCompletedCourse&& givesCertificate) {
+            generatecertificate();
+        }
+
+        function generatecertificate() {
+            console.log('Generating certificate....');
+        }
+     
+   let hasSolvedChallenge = false,
+        hasHintsLeft = false;
+    if(!hasSolvedChallenge && !hasHintsLeft){
+            showSolution();
+    }         
+
+   function showSolution() {
+       console.log("Show solution....");
+   } 
+
+   let likesDocumenteries = false,
+        likesStartUps = false;
+
+        if(!likesDocumenteries || likesStartUps) recomendMovie();
+    
+    function recomendMovie() {
+        console.log("Hey check out this new movie we think you might like");
+    }    
