e2d2ac065e5247546733d3b04068692b4b2a114c
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-11-13 00:29:35 +0000

    add ptactice

--- js-practice-sheet.js/sheet3/index.js
+++ js-practice-sheet.js/sheet3/index.js
@@ -1,8 +1,80 @@
-const copyBtn = document.getElementById("btn-copy")
+// let and const practice
+// Setting the stage
 
-copyBtn.addEventListener("click", async() => {
-    const content = document.getElementById("content-copy").textContent
-    await navigator.clipboard.writeText(content)
-    const copied = await navigator.clipboard.readText()
-    console.log(copied);
+const player = "Jack Norris"
+const opponent = "John Clovandam"
+const game =  "AmazingFighter"
+let points = 0
+let hasWon = false
+
+// PLAYING THE GAME
+points += 100
+hasWon = true
+
+// ANNOUNCING THE WINNER
+if (hasWon) {
+    console.log(`${player} got ${points} points and won the ${game} game`);
+} else {
+    console.log(`the winner is ${opponent}! ${player} lost the game`);
+}
+ // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+ // Log out items in an array
+
+ let myCourses = ["Learn CSS Animation", "UI Design Fundementals", "Intro to Clean Code"]
+
+ function itemsOfarray(arr) {
+     arr.filter(a => console.log(a))
+ }
+ itemsOfarray(myCourses)
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// Save to localStorage 
+
+let myFavoriteMovie = "Scarface"
+console.log(localStorage.getItem("myFavoriteMovie"));
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// addAventListener and object in an array
+
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    },
+    {
+        player: "Mark",
+        score: 41
+    }
+]
+
+const janesScoreButton = document.getElementById("janes-score-btn")
+let data1 = document.getElementById("data")
+
+
+janesScoreButton.addEventListener("click", () => {
+    data1.textContent += `${data[0].player}'s Score ${data[0].score}`
 })
+//:::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// The genetateSentence(desc, arr) takes two parameter: a description and an array.
+
+// It should return a string based upon the description and array
+
+// Rcsmple 1: if you pass in "largest countries", and ["China", "India", "USA"]
+
+// Example 2: If you pass in "best fruits and" and ["Apple", "Banana"], it should return: "The best fruits are Apple and Banana"
+
+// Use both for loop and letiral string to solve the challenge
+
+function genetateSentence(desc, arr) {
+    let s= []
+    for(let i = 0; i < arr.length; i++){
+      s.push(` ${arr[i]}`)
+    }
+    return `The ${arr.length} ${desc} are${s}`
+}
+
+console.log(genetateSentence("best fruits", ["Apple", "Banana"]));
+
+console.log(genetateSentence("largest countries",["China", "India", "USA"]));
