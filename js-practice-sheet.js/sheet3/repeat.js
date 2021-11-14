e51d22a0ca81e52822882a7316004c50647361fa
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-11-14 00:38:00 +0000

    Added more js practice

---
+++ js-practice-sheet.js/sheet3/repeat.js
@@ -0,0 +1,33 @@
+const myLeads = []
+const inputBtn = document.getElementById("input-el")
+const ulEl = document.getElementById("el-el")
+const saveTab = document.getElementById("save-tab")
+const deleteBtn = document.getElementById("delete-el")
+
+let myLeadsFromLocalStorage = localStorage.setItem(JSON.stringify("myLeads", "myLeads"))
+
+if(localStorage){
+    myLeadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    leads.map(a => listItems += `<li><a href="${a}">${a}</a></li>`)
+    ulEl.innerHTML = listItems
+}
+
+inputBtn.addEventListener("click", () => {
+    myLeads.push(ulEl.value)
+    localStorage.setItem("myLeads", JSON.stringify("myLeads"))
+    render(myLeads)
+})
+
+saveTab.addEventListener("click", () => {
+    chrome.tabs.query({active: true, currentWindow: true},(currentTab) =>{
+        currentTab[0].url
+    })
+    localStorage.setItem("myLeads", JSON.stringify("myLeads"))
+    render(myLeads)
+})
+
